2c05931839b4c1035e4135824de225443cec5897
Vivi524max <860736280@qq.com>
2026-06-10 08:20:46 +0800

    experiments: add baseline scan24 training log

--- scripts/run_baseline_scan105.sh
+++ scripts/run_baseline_scan105.sh
@@ -1,8 +1,8 @@
 set -e
-DTU_ROOT=${DTU_ROOT:-$HOME/data/2dgs/dtu}
+DTU_ROOT=${DTU_ROOT:-$HOME/data/2dgs/DTU}
 SCENE=scan105
 OUT=output/baseline/${SCENE}
 mkdir -p output/baseline logs results/baseline
 nvidia-smi
-CUDA_HOME=$CONDA_PREFIX PATH=$CONDA_PREFIX/bin:$PATH LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH python train.py -s ${DTU_ROOT}/${SCENE} -m ${OUT} -r 2 --depth_ratio 1 --iterations 10000 --save_iterations 3000 5000 8000 10000 --checkpoint_iterations 3000 5000 8000 10000
+TORCH_LIB=$(python -c "import torch, os; print(os.path.join(os.path.dirname(torch.__file__), 'lib'))") CUDA_HOME=$CONDA_PREFIX PATH=$CONDA_PREFIX/bin:$PATH LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$TORCH_LIB:$LD_LIBRARY_PATH python train.py -s ${DTU_ROOT}/${SCENE} -m ${OUT} -r 2 --depth_ratio 1 --iterations 10000 --save_iterations 3000 5000 8000 10000 --checkpoint_iterations 3000 5000 8000 10000
 nvidia-smi
